68fb36fc3a13c306173b96704e8611bad4f687e8
aldirif <97742642+AldiRif@users.noreply.github.com>
2022-12-13 11:18:08 +0700

    Initial commit

---
+++ src/MyFirstProgram.java
@@ -0,0 +1,20 @@
+public class MyFirstProgram {
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        System.out.println("Next Comment");
+        myFavoriteFood();
+        myFavoritePlace();
+    }
+
+    public static void myFavoriteFood(){
+        System.out.println("My Favorite food:");
+        System.out.println("1. Bakso");
+        System.out.println("2. Mei Goreng Tante (Tanpa Telor)");
+        System.out.println("3. Mei Ayam");
+    }
+    public static void myFavoritePlace() {
+        System.out.println("My Favorite place:");
+        System.out.println("1. Wonderland");
+    }
+}
